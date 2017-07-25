da80cdd586a0f04a759a807f8dd58f1d02ef483b
marleen berg <marleenberg1990@gmail.com>
2017-07-25 23:25:50 +0200

    rename

--- src/nl/marleenberg/javase7/chapter1/javastructure/JavaStructure.java
+++ src/nl/marleenberg/javase7/chapter1/javastructure/JavaStructure.java
@@ -4,7 +4,7 @@ package nl.marleenberg.javase7.chapter1.javastructure;
 import nl.marleenberg.javase7.chapter1.student.Person;
 import nl.marleenberg.javase7.chapter1.student.Student;
 
-public class JavaStructuur {
+public class JavaStructure {
     public static void main(String[] args) {
         Person pers = new Person(12, "Klaas", "Lutjebroek");
         Student student = new Student(43, "Piet", "Staphorst", "Java");
@@ -13,5 +13,4 @@ public class JavaStructuur {
         System.out.printf("%s\n", student.toString());
 
     }
-
 }
